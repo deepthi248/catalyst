eeb2c3802ecef752ae427c3d34ae7325c73cb1eb
Deepthi Bhogaraju <deepthibhogaraju.3@gmail.com>
2026-06-27 17:12:49 +0530

    Database(SQL Lite) setup

---
+++ server/db.js
@@ -0,0 +1,34 @@
+import Database from "better-sqlite3";
+const db = new Database("./catalyst.db");
+
+const create_table_query = `CREATE TABLE IF NOT EXISTS jobs (
+  id          TEXT PRIMARY KEY,
+  user_id     TEXT NOT NULL,
+  company     TEXT NOT NULL,
+  role        TEXT NOT NULL,
+  status      TEXT NOT NULL,
+  url         TEXT,
+  notes       TEXT,
+  jd_text     TEXT,
+  created_at  TEXT NOT NULL
+)`;
+
+const dummy_insert_query = `
+  INSERT INTO jobs (id, user_id, company, role, status, url, notes, jd_text, created_at)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+`
+
+ 
+
+db.prepare(dummy_insert_query).run( 'test-001',
+  'user-001', 
+  'Stripe',
+  'Software Engineer',
+  'applied',
+  'https://stripe.com',
+  null,
+  null,
+  new Date().toISOString()
+)
+db.exec(create_table_query);
+export default db;
